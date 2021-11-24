fbc8169e50eee625885aa08f61016003f7ac1511
lrtwr <jeroen.leertouwer@outlook.com>
2021-11-24 16:48:51 +0100

    !

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { MenuComponent } from './menu/menu/menu.component';
+import { MenuItemComponent } from './menu/menu-item/menu-item.component';
+import { MenuModule } from './menu/menu.module';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    MenuModule
+    ,AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
